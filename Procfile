eebc23fa37776f5eebcb248d279039fa4fc28855
Daniel Creagh <daniel@creagh.com>
2015-07-14 16:15:41 +0100

    fixing it

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node scripts/web.js
+web: node web.js
--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + 'index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.use(express.static(__dirname + '/'))
